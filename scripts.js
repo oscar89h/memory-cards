10cdfec3561df745caa4c5df8f8437062ac34769
Oscar Zavala <oscaromar89@yahoo.com>
2022-10-23 02:51:30 -0600

    cuarto commit

--- scripts.js
+++ scripts.js
@@ -1,9 +1,12 @@
-const frontArrow = document.querySelector(".fa-arrow-left");
-const backArrow = document.querySelector(".fa-arrow-right");
+const frontArrow = document.querySelector(".fa-arrow-right");
+const backArrow = document.querySelector(".fa-arrow-left");
 const cardsContainer = document.querySelector(".cards-container");
 const innerCard = document.querySelector(".inner-card");
 
-let currentCard = 0;
+const addCardContainer = document.querySelector('.add-card-container')
+const btnSmall = document.querySelector('.btn-small')
+
+let currentCardIndex = 0;
 let cardsArr = [];
 
 let cardsObjArr = [
@@ -19,14 +22,19 @@ let cardsObjArr = [
       question: "que es Node ",
       answer: "Node es el lenguaje de programacion que permite correr Javascript fuera de una pagina web",
    },
+   {
+      question: "Que es PHP ",
+      answer: "Es un lenguaje de programacion",
+   },
 ];
-
-
+btnSmall.addEventListener('click', showAddCard)
+frontArrow.addEventListener("click", showNextCard);
+backArrow.addEventListener("click", showPreCard);
 
 function updateDOM() {
    cardsObjArr.forEach((cardObj) => {
       const card = document.createElement("div");
-      card.classList.add('card')
+      card.classList.add("card");
       card.innerHTML = `
         <div class="inner-card ">
             <div class="inner-card-front">
@@ -37,41 +45,69 @@ function updateDOM() {
         
         `;
 
-        cardsArr.push(card)
+      cardsArr.push(card);
 
-
-      populateDOM()     
-
+      populateDOM();
    });
-
-
-
 }
 
-updateDOM() 
+function showNextCard() {
+   cardsArr[currentCardIndex].className = "card left";
+
+   currentCardIndex = currentCardIndex + 1;
+
+   if (currentCardIndex > cardsArr.length - 1) {
+      currentCardIndex = cardsArr.length - 1;
+   }
+   cardsArr[currentCardIndex].className = " card active";
+}
+
+function showPreCard() {
+   cardsArr[currentCardIndex].className = "card left";
+
+   currentCardIndex = currentCardIndex - 1;
+
+   if (currentCardIndex < 0) {
+      currentCardIndex = 0;
+   }
+   cardsArr[currentCardIndex].className = " card active";
+}
+
+updateDOM();
 
 function populateDOM() {
    cardsArr.forEach((cardInArr, index) => {
-      if(index === 0) {
-         cardInArr.classList.add ('active')
+      if (index === 0) {
+         cardInArr.classList.add("active");
       }
 
+      cardsContainer.appendChild(cardInArr);
+   });
 
-      cardsContainer.appendChild(cardInArr)
-   })
+   const cards = document.querySelectorAll(".card");
 
-   const cards = document.querySelectorAll('.card')
-
-   cards.forEach(card => {
-      card.addEventListener('click', flipCard)
-   })
-   
+   cards.forEach((card) => {
+      card.addEventListener("click", flipCard);
+   });
 }
- 
+
+// mostrar siguiente tarjeta
+
 function flipCard() {
-   this.classList.toggle('show-answer')
+   this.classList.toggle("show-answer");
 }
-   
 
 
-console.log(cardsArr[0])
+function showAddCard() {
+addCardContainer.style.display = 'flex'
+const closeAddCardContainer = document.querySelector('.btn-ghost')
+
+closeAddCardContainer.addEventListener('click', closeAddCardContainerFunc)
+
+}
+
+
+function closeAddCardContainerFunc() {
+addCardContainer.style.display = 'none'
+
+}
